d51bb6d508dde687eaf8b74b66b0b8c7fe0e88f3
Andrej-Gorlov <avgorlov899@gmali.com>
2023-04-13 21:10:12 +0300

    edit textarea

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ function TextareaChangesStyle() {
   document
     .getElementById("id_label_textarea")
     .classList.add("label_textarea_focus");
+  document.getElementById("id_textarea").style =
+    "height:150px;background: #ebf3ff; ";
+  document.getElementById("borderId").classList.add("border_textarea_custom");
+  document
+    .getElementById("id_span_label")
+    .classList.remove("span_label_textarea");
 }
 
 function TextareaEmpty() {
@@ -12,6 +18,13 @@ function TextareaEmpty() {
     document
       .getElementById("id_label_textarea")
       .classList.add("label_textarea");
+    document.getElementById("id_textarea").style = "height:15px;";
+    document
+      .getElementById("borderId")
+      .classList.remove("border_textarea_custom");
+    document
+      .getElementById("id_span_label")
+      .classList.add("span_label_textarea");
   }
 }
 function LoadTextarea() {
